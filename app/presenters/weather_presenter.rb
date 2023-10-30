08396937a829ac63ebceb19e8baa1cf1ffd7803c
Sai dileep Reddy <saidileepreddyaella@gmail.com>
2023-10-30 09:23:00 +0530

    update weather presenter

--- app/presenters/weather_presenter.rb
+++ app/presenters/weather_presenter.rb
@@ -13,15 +13,19 @@ class WeatherPresenter
   end
 
   def temperature
-    "#{@weather_data['main']['temp']}°C"
+    "#{@weather_data['main']['temp']}"
+  end
+
+  def temperature_feels_like
+    "#{@weather_data['main']['feels_like']}"
   end
 
   def high_temperature
-    "#{@weather_data['main']['temp_max']}°C"
+    "#{@weather_data['main']['temp_max']}"
   end
 
   def low_temperature
-    "#{@weather_data['main']['temp_min']}°C"
+    "#{@weather_data['main']['temp_min']}"
   end
 
   def weather_description
